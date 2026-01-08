e7aa03036172a9fb942aedd1423d8c3866c382ae
Soy(Lee SoHyeon) <127467306+teabag759@users.noreply.github.com>
2026-01-08 13:39:37 +0900

    setting for dev

---
+++ src/main/java/com/soy/ebpractice/controller/MenuController.java
@@ -0,0 +1,13 @@
+package com.soy.ebpractice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MenuController {
+
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "I'm alive";
+    }
+}
