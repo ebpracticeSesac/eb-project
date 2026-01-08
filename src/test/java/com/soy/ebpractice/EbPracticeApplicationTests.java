7a6b20b66beacd1c2e34505b5c1b55d5fb2caa1c
Soy(Lee SoHyeon) <127467306+teabag759@users.noreply.github.com>
2026-01-08 12:34:59 +0900

    first commit

---
+++ src/test/java/com/soy/ebpractice/EbPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soy.ebpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
