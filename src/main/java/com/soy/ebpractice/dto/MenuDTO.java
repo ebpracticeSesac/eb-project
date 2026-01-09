917f18bded1c09e9a3c221dd2f5ee7bdd4bbb6b2
Soy(Lee SoHyeon) <127467306+teabag759@users.noreply.github.com>
2026-01-09 09:58:26 +0900

    model mapper

---
+++ src/main/java/com/soy/ebpractice/dto/MenuDTO.java
@@ -0,0 +1,14 @@
+package com.soy.ebpractice.dto;
+
+
+public class MenuDTO {
+    private int menuCode;
+
+    private String menuName;
+
+    private String menuPrice;
+
+    private String categoryCode;
+
+    private String orderableStatus;
+}
